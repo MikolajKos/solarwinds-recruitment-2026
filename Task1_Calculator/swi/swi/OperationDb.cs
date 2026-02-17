f28e3d2e7b9394afb71ea57d16b2df997307f2ce
MikolajKos <niko.kosiorek@gmail.com>
2026-02-17 12:15:06 +0100

    Initial json read and structure implemented

---
+++ Task1_Calculator/swi/swi/OperationDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace swi
+{
+    public class SingleOperation
+    {
+        [JsonPropertyName("operator")]
+        public string OperationType { get; set; } = string.Empty;
+
+        [JsonPropertyName("value1")]
+        public double Value1 { get; set; }
+
+        [JsonPropertyName("value2")]
+        public double? Value2 { get; set; }
+    }
+
+    public static class OperationDb
+    {
+        public static Dictionary<string, SingleOperation>? database;
+
+        public static void Deserialize(string file)
+        {
+            string jsonContent = File.ReadAllText(file);
+            database = JsonSerializer.Deserialize<Dictionary<string, SingleOperation>>(jsonContent);
+        }
+    }
+}
---
+++ Task1_Calculator/swi/swi/Program.cs
@@ -0,0 +1,11 @@
+﻿using swi;
+
+OperationDb.Deserialize("input.json");
+var db = OperationDb.database;
+
+if (db == null) { return; }
+
+foreach (var item in db)
+{
+    Console.WriteLine(item.Key);
+}
